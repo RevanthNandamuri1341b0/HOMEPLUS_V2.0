898a27cba6e276fe7fbbe34fa7fec1eaca04e37f
RevanthSai Nandamuri <revanth.nandamuri1262000@gmail.com>
2021-02-26 09:57:20 +0530

    code update

--- Code/HOME__2.0__with_watering_system_/HOME__2.0__with_watering_system_.ino
+++ Code/HOME__2.0__with_watering_system_/HOME__2.0__with_watering_system_.ino
@@ -18,8 +18,10 @@ Date of last update:- 25/02/2021
 #include <Adafruit_Sensor.h>
 char auth[] = "";     //CREATED IN THE APPLICATION
 char ssid[] = "";
-char pass[] = "9391198850";
+char pass[] = "";
 #define DHTPIN 2          // Digital pin 4
+
+//Uncomment the type of sensor which of your choice and coment the rest
 //#define DHTTYPE DHT11     // DHT 11
 #define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
 //#define DHTTYPE DHT21   // DHT 21, AM2301
